d10b4ad85e48f0cee3e948c43eefffc02b0da364
potapchukdmytro <traiget@gmail.com>
2025-05-17 11:21:39 +0300

    product controller

---
+++ team2/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace team2.Controllers
+{
+    [ApiController]
+    [Route("api/product")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
